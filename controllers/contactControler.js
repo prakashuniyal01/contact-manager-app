e46464b24161f0186c8b2f635332c2aec6d836a0
prakashuniyal01 <uniyalprakash7@gmail.com>
2023-03-14 10:57:06 +0530

    db  connection

--- controllers/contactControler.js
+++ controllers/contactControler.js
@@ -1,25 +1,28 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 // @route get /api/conacts
 
 const getContacts =  asyncHandler( async(req, res) =>{
-    res.status(200).json({
-        message:"get all contect"    
-    });
+    const conacts = await Contact.find();
+    res.status(200).json(conacts);
 });
 
 // route post /api/conacts 
 
 const createContact = asyncHandler( async(req, res) =>{
     console.log(req.body);
-    const {name, email , phoneNumber} =req.body;
-    if(!name ||  !email  || !phoneNumber){
+    const {name, email , phone} =req.body;
+    if(!name ||  !email  || !phone){
         res.status(400);
         // massage:"All fields are mandatory"
         throw new Error ("all fields are mandatory");
     }
-    res.status(201).json({
-        message:"create  contect"    
-    });
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    })
+    res.status(201).json(contact);
 });
 
 
@@ -27,6 +30,11 @@ const createContact = asyncHandler( async(req, res) =>{
 
 
 const getContact = asyncHandler( async(req, res ) =>{
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(400);
+        throw new Error("Contact not found ");
+    }
     res.status(200).json({
         message:`get contact for ${req.params.id}`
     });
